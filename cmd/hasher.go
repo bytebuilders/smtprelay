3e91b234e33e646378ab1a5928513261d117b4a1
Tamal Saha <tamal@appscode.com>
2025-02-01 21:02:59 +0600

    URL generator
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- cmd/hasher.go
+++ cmd/hasher.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"fmt"
+	"net/url"
 	"os"
 
 	"golang.org/x/crypto/bcrypt"
 )
 
-func main() {
+func main_() {
 	if len(os.Args) != 2 {
 		fmt.Fprintln(os.Stderr, "Usage: hasher PASSWORD")
 		os.Exit(1)
@@ -19,4 +20,15 @@ func main() {
 		fmt.Fprintf(os.Stderr, "Error generating hash: %s\n", err)
 	}
 	fmt.Println(string(hash))
+}
+
+func main() {
+	var u url.URL
+	u.Scheme = "starttls"
+	u.User = url.UserPassword("emailapikey", "admin")
+	u.Host = "smtp.zeptomail.com:587"
+	qr := url.Values{}
+	qr.Add("auth", "login")
+	u.RawQuery = qr.Encode()
+	fmt.Println(u.String())
 }
